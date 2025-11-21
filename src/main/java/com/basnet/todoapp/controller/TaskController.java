b8b88d50c8ce15a994dde06eede4c1ff163497a8
Sisir1232 <sisirb77@gmail.com>
2025-11-21 14:04:40 +0545

    get operation by finding the all tasks and task by id

--- src/main/java/com/basnet/todoapp/controller/TaskController.java
+++ src/main/java/com/basnet/todoapp/controller/TaskController.java
@@ -1,10 +1,14 @@
 package com.basnet.todoapp.controller;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.basnet.todoapp.entities.Task;
+import com.basnet.todoapp.exception.ResourceNotFoundException;
 import com.basnet.todoapp.service.TaskService;
 
 @RestController
@@ -15,6 +19,16 @@ public class TaskController {
     public Task  createTask(@RequestBody Task task){
         return taskService.saveTask(task);
  
+    }
+    @GetMapping("/task")
+    public List<Task> getTasks(){
+        return taskService.getTasks();
+    }
+     @GetMapping("/task/{id}")
+    public Task getTaskById( @PathVariable Long id){
+        return taskService.getTaskById(id);
+        
+        
     }
 
 }
--- src/main/java/com/basnet/todoapp/service/TaskService.java
+++ src/main/java/com/basnet/todoapp/service/TaskService.java
@@ -1,9 +1,13 @@
 package com.basnet.todoapp.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.basnet.todoapp.entities.Task;
+import com.basnet.todoapp.exception.ResourceNotFoundException;
 import com.basnet.todoapp.repo.TaskRepo;
 
 @Service
@@ -13,5 +17,12 @@ public class TaskService {
 	public Task  saveTask(Task task) {
 		return taskRepo.save(task);
 	}
+    public List<Task> getTasks() {
+       return  taskRepo.findAll();
+    }
+    public Task getTaskById(Long id) {
+       return  taskRepo.findById(id)
+                      .orElseThrow(()->new ResourceNotFoundException("not found with id "+ id));
+    }
 
 }
